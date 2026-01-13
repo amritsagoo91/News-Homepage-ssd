94bc55e35f89b886772948f37dbfbe89537ab2c3
samankayhanian <saman@kayhanian.com>
2026-01-12 18:32:42 -0800

    12-Jan-update

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@
 // Responsive Menu - Dropdown
 const body = document.body;
 const btnMenu = document.getElementById("btn-menu");
-const bar = document.getElementById("bar");
 const nav = document.getElementById("main-navigation");
 const backBtn = document.getElementById("back-btn");
 
 btnMenu.addEventListener("click", openMenu);
+
 // Prevents the focus state from activating
 btnMenu.addEventListener("mousedown", function (e) {
   e.preventDefault();
